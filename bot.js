5be3d52ef2ba9c35fd9b35c34ea7d1cf87f19ce3
Ezequiel <ekogut@gmail.com>
2018-12-22 12:51:01 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -107,7 +107,8 @@ client.on("message", (message) => {
 	 
 	if( (message.channel.name == nombreCom) && (message.guild.channels.find('name', nombreLog)!== null) ){ 
 		if(message.content.substr(0,3).toUpperCase() == "CLI"){	console.log(client)};
-		if(message.content.substr(0,3).toUpperCase() == "GIL"){	console.log(client.guilds[0], "###############################",client.guilds[1], "###############################",client.guilds[2], "###############################")};
+		if(message.content.substr(0,3).toUpperCase() == "GUI"){	console.log(client.guilds)};		
+		if(message.content.substr(0,3).toUpperCase() == "GIL"){	console.log(client.guilds.map(function(item) { return item["members"]; })};
 		if(message.content.substr(0,3).toUpperCase() == "GIS"){	message.channel.send(client.guilds.map(function(item) { return item["name"]; }))};
 		
 	}
